a4e22ec89590d8abba725a5ff99167b7dad517e3
Yohann Mehboob <yohann@setcourse.com>
2024-12-31 22:21:51 -0600

    Adding project to github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe></TicTacToe>
+    </div>
+  );
+}
+
+export default App;
